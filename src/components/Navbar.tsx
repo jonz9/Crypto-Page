92f555ee6bb82bb858a62a04166726c6a197e743
j444zhan <j444zhan@uwaterloo.ca>
2024-01-17 02:02:31 -0500

    initialized files

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='justify-between  flex gap-10 bg-black'>
+      <img src='' alt='logo' className='w-45 h-20' />
+      
+    </div>
+  )
+}
+
+export default Navbar
