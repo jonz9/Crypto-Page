1a6313c7709a73bc416c63d9d0817caa17208b24
j444zhan <j444zhan@uwaterloo.ca>
2024-01-17 02:45:49 -0500

    added logo, navbar, buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Logo from '../assets/Logo.png';
+import { Button } from './Button';
 
 const Navbar = () => {
   return (
-    <div className='justify-between  flex gap-10 bg-black'>
-      <img src='' alt='logo' className='w-45 h-20' />
+    <div className='justify-between flex gap-10 bg-black'>
+      <img src={Logo} alt='logo' className='w-40 h-15 m-3' />
+      <div className='flex justify-center items-center gap-4 m-4'>
+        <Button>Log In</Button>
+        <Button>Register</Button>
+      </div>
       
     </div>
   )
