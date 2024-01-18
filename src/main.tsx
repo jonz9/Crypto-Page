029f3af6f24e7d2075c8f5d8aecf3795ced43c80
j444zhan <j444zhan@uwaterloo.ca>
2024-01-18 10:08:50 -0500

    added clerk auth dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const root = createRoot(document.getElementById("root")!);
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey="PUBLISHABLE_KEY">
+      <App />
+    </ClerkProvider>
   </React.StrictMode>
 );
